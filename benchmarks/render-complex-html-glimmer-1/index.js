4e72453cf93a2c097d9c773c8e2fbff812e6f6f7
Sérgio Saquetim <saquetim@discourse.org>
2024-07-25 14:19:09 -0300

    DEV: Added benchmarks for different number of items on Glimmer Complex List

---
+++ benchmarks/render-complex-html-glimmer-1/index.js
@@ -0,0 +1,71 @@
+/* global RenderTemplateTestClient */
+
+(function () {
+  RenderTemplateTestClient.run({
+    name: "Render Complex List (Glimmer) - 1 item",
+
+    setup() {
+      const EmberObject = require("@ember/object").default;
+      const computed = require("@ember/object").computed;
+      const A = require("@ember/array").A;
+
+      const MyThing = EmberObject.extend({
+        d: computed("a", "b", function () {
+          return this.get("a") + this.get("b");
+        }),
+      });
+
+      let listItems = A([]);
+      for (let i = 0; i < 1; i++) {
+        listItems.pushObject(
+          MyThing.create({
+            a: "a" + i,
+            b: "b" + i,
+            c: "c" + i,
+          }),
+        );
+      }
+
+      this.setupTemplateTest(
+        "complex-list-main-wrapped",
+        { items: listItems },
+        { componentMode: "glimmer" },
+      );
+
+      this.registry.register(
+        "template:components/component-render",
+        this.template("complex-list-component-wrapped"),
+      );
+      this.registry.register(
+        "template:components/nested-component",
+        this.template("complex-list-nested-wrapped"),
+      );
+      this.registry.register(
+        "template:components/buffer-render",
+        this.template("complex-list-data-wrapped"),
+      );
+
+      const GlimmerComponent = require("@glimmer/component").default;
+      this.registry.register(
+        "component:component-render",
+        class extends GlimmerComponent {},
+      );
+      this.registry.register(
+        "component:nested-component",
+        class extends GlimmerComponent {},
+      );
+      this.registry.register(
+        "component:buffer-render",
+        class extends GlimmerComponent {},
+      );
+    },
+
+    reset() {
+      this.hideComponent();
+    },
+
+    test() {
+      this.showComponent();
+    },
+  });
+})();

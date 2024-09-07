68329e624c616981f09fc2ad49539f0b5d849205
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-09-07 19:07:59 -0400

    Add 5-10 local-glimmer

---
+++ app-at-version/ember-5-10-local-glimmer/ember-cli-build.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = async function (defaults) {
+  const utils = await import('ember-cli-utils');
+  const config = await utils.configure(__dirname, ['common']);
+
+  const app = new EmberApp(defaults, {
+    // Add options here
+    ...config,
+  });
+
+  if (process.env.BUILD === 'classic') {
+    return app.toTree();
+  }
+
+  const { Webpack } = require('@embroider/webpack');
+
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticHelpers: true,
+    staticModifiers: true,
+    staticComponents: true,
+    staticEmberSource: false,
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+    packagerOptions: {
+      webpackConfig: {
+        devtool: 'source-map',
+      },
+    },
+  });
+};

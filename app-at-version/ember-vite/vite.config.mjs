cf986654504c36490d301902d1b6542e272e4b6a
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-09-06 22:14:21 -0400

    I messed up something

--- app-at-version/ember-vite/vite.config.mjs
+++ app-at-version/ember-vite/vite.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 import {
   resolver,
   hbs,
@@ -8,28 +8,24 @@ import {
   compatPrebuild,
   assets,
   contentFor,
-} from '@embroider/vite';
-import { resolve } from 'path';
-import { babel } from '@rollup/plugin-babel';
-
-const root = 'tmp/rewritten-app';
-const extensions = [
-  '.mjs',
-  '.gjs',
-  '.js',
-  '.mts',
-  '.gts',
-  '.ts',
-  '.hbs',
-  '.json',
-];
+} from "@embroider/vite";
+import { resolve } from "path";
+import { babel } from "@rollup/plugin-babel";
 
 export default defineConfig(({ mode }) => {
   return {
-    root,
-    cacheDir: resolve('node_modules', '.vite'),
+    cacheDir: resolve("node_modules", ".vite"),
     resolve: {
-      extensions,
+      extensions: [
+        ".mjs",
+        ".gjs",
+        ".js",
+        ".mts",
+        ".gts",
+        ".ts",
+        ".hbs",
+        ".json",
+      ],
     },
     plugins: [
       hbs(),
@@ -41,32 +37,26 @@ export default defineConfig(({ mode }) => {
       contentFor(),
 
       babel({
-        babelHelpers: 'runtime',
-        extensions,
+        babelHelpers: "runtime",
+
+        // this needs .hbs because our hbs() plugin above converts them to
+        // javascript but the javascript still also needs babel, but we don't want
+        // to rename them because vite isn't great about knowing how to hot-reload
+        // them if we resolve them to made-up names.
+        extensions: [".gjs", ".js", ".hbs", ".ts", ".gts"],
       }),
     ],
     optimizeDeps: optimizeDeps(),
-    publicDir: resolve(process.cwd(), 'public'),
     server: {
       port: 4200,
-      watch: {
-        ignored: ['!**/tmp/rewritten-app/**'],
-      },
     },
     build: {
-      outDir: resolve(process.cwd(), 'dist'),
+      outDir: "dist",
       rollupOptions: {
         input: {
-          main: resolve(root, 'index.html'),
-          ...(shouldBuildTests(mode)
-            ? { tests: resolve(root, 'tests/index.html') }
-            : undefined),
+          main: "index.html",
         },
       },
     },
   };
 });
-
-function shouldBuildTests(mode) {
-  return mode !== 'production' || process.env.FORCE_BUILD_TESTS;
-}

bc27f62920d93f193fdc784d1ca23bff717df744
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-03-11 18:50:55 -0400

    It repros differently

--- app-at-version/ember-canary-vite/vite.config.mjs
+++ app-at-version/ember-canary-vite/vite.config.mjs
@@ -2,22 +2,22 @@ import { defineConfig } from 'vite';
 import { extensions, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 
-const validator = `${process.cwd()}/node_modules/ember-source/dist/packages/@glimmer/validator/index.js`;
-const tracking = `${process.cwd()}/node_modules/ember-source/dist/packages/@glimmer/tracking/index.js`;
-const cachePrimitives = `${process.cwd()}/node_modules/ember-source/dist/packages/@glimmer/tracking/primitives/cache.js`;
+// const validator = `${process.cwd()}/node_modules/ember-source/dist/packages/@glimmer/validator/index.js`;
+// const tracking = `${process.cwd()}/node_modules/ember-source/dist/packages/@glimmer/tracking/index.js`;
+// const cachePrimitives = `${process.cwd()}/node_modules/ember-source/dist/packages/@glimmer/tracking/primitives/cache.js`;
 
 export default defineConfig({
   base: '/ember-canary-vite/',
   resolve: {
     extensions,
-    alias: [
-      { find: /^@glimmer\/validator$/, replacement: validator },
-      { find: /^@glimmer\/tracking$/, replacement: tracking },
-      {
-        find: /^@glimmer\/tracking\/primitives\/cache$/,
-        replacement: cachePrimitives,
-      },
-    ],
+    // alias: [
+    //   { find: /^@glimmer\/validator$/, replacement: validator },
+    //   { find: /^@glimmer\/tracking$/, replacement: tracking },
+    //   {
+    //     find: /^@glimmer\/tracking\/primitives\/cache$/,
+    //     replacement: cachePrimitives,
+    //   },
+    // ],
   },
   plugins: [
     ember(),
